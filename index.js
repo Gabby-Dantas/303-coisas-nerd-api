8aad0d9a00168aec06b997edd100e0cf1832b27c
Bruno Pimentel <brunoapimentel@gmail.com>
2018-03-09 16:47:25 -0300

    Adiciona gravação de dados

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const expressMongoDb = require('express-mongo-db');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
 app.use(expressMongoDb('mongodb://localhost/coisas-nerd'));
+app.use(bodyParser.json());
 
 app.get('/anime', function(req, res){
     req.db.collection('anime').find().toArray(function(erro, dados){
@@ -12,8 +14,20 @@ app.get('/anime', function(req, res){
     });
 });
 
+app.post('/anime', function(req, res){
+    req.db.collection('anime').insert(req.body, function(erro, dados){
+        res.send(dados);
+    });
+});
+
 app.get('/cartoon', function(req, res){
     req.db.collection('cartoon').find().toArray(function(erro, dados){
+        res.send(dados);
+    });
+});
+
+app.post('/cartoon', function(req, res){
+    req.db.collection('cartoon').insert(req.body, function(erro, dados){
         res.send(dados);
     });
 });
